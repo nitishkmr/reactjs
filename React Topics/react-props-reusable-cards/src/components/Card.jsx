ece7130d3d42a30b4c89637ddc3a8ede6514d4f7
Nitish Kumar <nitishkmr096@gmail.com>
2021-04-07 02:20:10 +0530

    Add Keeper-v1 and other React Topics-Cards

---
+++ React Topics/react-props-reusable-cards/src/components/Card.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Detail from "./Detail";
+
+function Card(props) {
+    return (
+      <div>
+        <div className="card">
+          <div className="top">
+            <h2 className="name">{props.name}</h2>
+            <img
+              className="circle-img"
+              src={props.imgURL}
+              alt="avatar_img"
+            />
+          </div>
+          <div className="bottom">
+            <Detail detailInfo={props.phone}/>
+            <Detail detailInfo={props.email}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  export default Card;
---
+++ keeper-app-part-1/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>
+                Copywrite ⓒ {new Date().getFullYear()}
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
